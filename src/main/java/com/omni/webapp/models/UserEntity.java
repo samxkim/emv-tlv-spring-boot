e33aef0e9c25bdc0cffbd4da27bd7a481ea4cca6
samxkim <samxkim@gmail.com>
2021-09-27 21:18:30 -0700

    UserEntity

--- src/main/java/com/omni/webapp/models/UserEntity.java
+++ src/main/java/com/omni/webapp/models/UserEntity.java
@@ -27,5 +27,45 @@ public class UserEntity implements Serializable {
     private String email;
 
     private String encryptedPassword;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
 }
 
--- src/main/java/com/omni/webapp/service/UserServiceImpl.java
+++ src/main/java/com/omni/webapp/service/UserServiceImpl.java
@@ -1,13 +1,33 @@
 package com.omni.webapp.service;
 
 import com.omni.webapp.models.UserDto;
+import com.omni.webapp.models.UserEntity;
+import com.omni.webapp.models.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("test");
+        userEntity.setUserName("test1");
+        userEntity.setEmail("test@test.com");
+
+        UserEntity userDetails = userRepository.save(userEntity);
+
+        UserDto returnUser = new UserDto();
+        BeanUtils.copyProperties(userDetails, returnUser);
+
+        return returnUser;
     }
 }
