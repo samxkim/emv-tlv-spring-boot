c53aea4d3008f62885449f2781881b0841425053
samxkim <samxkim@gmail.com>
2021-09-27 21:01:26 -0700

    UserDto

---
+++ src/main/java/com/omni/webapp/models/UserDto.java
@@ -0,0 +1,70 @@
+package com.omni.webapp.models;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+    private static final long serialVersionUID = -8851311327936217725L;
+    private long id;
+    private String userId;
+    private String userName;
+    private String email;
+    private String companyName;
+    private String password;
+    private String encryptedPassword;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+}
