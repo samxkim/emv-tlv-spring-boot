07079f6375a74827d6058d89a334db9010fcec1b
Samuel Kim <samxkim@gmail.com>
2021-10-21 10:17:00 -0700

    Added bearer authorization option to requests from swagger ui

--- src/main/java/com/omni/webapp/config/SwaggerConfig.java
+++ src/main/java/com/omni/webapp/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.omni.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,5 +25,21 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Primary
+    @Bean
+    public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider) {
+        return () -> {
+            SwaggerResource wsResource = new SwaggerResource();
+            wsResource.setName("Documentation");
+            wsResource.setSwaggerVersion("3.0.3");
+            wsResource.setLocation("/swagger-config.yaml");
+
+            List<SwaggerResource> resources = new ArrayList<>(defaultResourcesProvider.get());
+            resources.clear();
+            resources.add(wsResource);
+            return resources;
+        };
     }
 }
